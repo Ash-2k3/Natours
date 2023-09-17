8cae533ddf4640f0cbb2c8685895f8dc186f7299
Ash-2k3 <ash.me.cse@gmail.com>
2023-09-17 12:16:03 +0530

    Handle get request: read data from .json file and send back the response

--- app.js
+++ app.js
@@ -1,15 +1,30 @@
 const express = require('express');
+const fs = require('fs')
 
 const app = express()
 
-app.get('/', (req, res) => {
-           res.status(200).json(
-                      {message: 'Hello from the server side!',
-           app: 'Notours'})
-})
+// app.get('/', (req, res) => {
+//            res.status(200).json(
+//                       {message: 'Hello from the server side!',
+//            app: 'Notours'})
+// })
 
-app.post('/', (req, res) => {
-           res.send('You can post to this endpoint..')
+// app.post('/', (req, res) => {
+//            res.send('You can post to this endpoint..')
+// })
+
+// Good practise to mention version of api: v1/v2 etc
+
+const tours = JSON.parse(fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`))
+
+app.get('/api/v1/tours', (req, res) => {
+           res.status(200).json({
+                      status: 'success',
+                      result: tours.length,
+                      data: {
+                                 tours: tours
+                      }
+           })
 })
 
 const port = 3000
