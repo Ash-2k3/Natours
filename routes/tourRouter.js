813c0c9f48cadc4afe3e99590f7ea25b589a686f
Ash-2k3 <ash.me.cse@gmail.com>
2023-09-20 16:54:24 +0530

    Param middlewares

--- routes/tourRouter.js
+++ routes/tourRouter.js
@@ -2,6 +2,11 @@ const express = require('express')
 const tourController = require('./../controllers/tourController')
 const router = express.Router()
 
+// Param Middleware
+router.param('id', (req, res, next, val) => {
+           console.log(`Tour id is ${val}`);
+           next()
+})
 
 // Mounting Routes
 router.route('/').get(tourController.getAllTours).post(tourController.createTour);
