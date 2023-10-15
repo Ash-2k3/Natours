9880ebc5711dcf6930537bf305e602bd6bb4aef5
Ash-2k3 <ash.me.cse@gmail.com>
2023-10-15 14:14:32 +0530

    Refactoring code into MVC pattern

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,34 +1,35 @@
 const fs = require('fs')
 const { nextTick } = require('process')
+const Tour = require('./../models/tourModel')
 
-const tours = JSON.parse(fs.readFileSync(`${__dirname}/../dev-data/data/tours-simple.json`))
+// const tours = JSON.parse(fs.readFileSync(`${__dirname}/../dev-data/data/tours-simple.json`))
 
 exports.getAllTours = (req, res) => {
            res.status(200).json({
                       status: 'success',
                       requestAt: req.requestTime,
-                      result: tours.length,
-                      data: {
-                                 tours: tours
-                      }
+                      // result: tours.length,
+                      // data: {
+                      //            tours: tours
+                      // }
            })
 }
 
 exports.createTour = (req, res) => {
            // console.log(req.body) // cuz of middleware body is accessible
 
-           const newId = tours[tours.length - 1].id + 1;
-           const newTour = Object.assign({id: newId}, req.body);
+           // const newId = tours[tours.length - 1].id + 1;
+           // const newTour = Object.assign({id: newId}, req.body);
 
-           tours.push(newTour);
-           fs.writeFile(`${__dirname}/dev-data/data/tours-simple.json`, JSON.stringify(tours), err => {
-                      res.status(201).json({
-                                 status: 'success',
-                                 data: {
-                                          tour: newTour  
-                                 }
-                      })
-           })
+           // tours.push(newTour);
+           // fs.writeFile(`${__dirname}/dev-data/data/tours-simple.json`, JSON.stringify(tours), err => {
+           //            res.status(201).json({
+           //                       status: 'success',
+           //                       data: {
+           //                                tour: newTour  
+           //                       }
+           //            })
+           // })
 
            // res.send('Done') // to end the res cycle
 }
@@ -37,51 +38,51 @@ exports.getTourById = (req, res) => {  // ? for optional
            // console.log(req.body) // cuz of middleware body is accessible
            console.log(req.params)
            
-           const id = req.params.id * 1
-           if (id > tours.length) {
-                      return res.status(404).json({
-                                 status: 'fail',
-                                 message: 'Invalid Id'
-                      })
-           }
+           // const id = req.params.id * 1
+           // if (id > tours.length) {
+           //            return res.status(404).json({
+           //                       status: 'fail',
+           //                       message: 'Invalid Id'
+           //            })
+           // }
            
-           const tour = tours.find(el => el.id === id)
-           res.status(200).json({
-                      status: 'success',
-                      data: {
-                                 tour
-                      }
-           })
+           // const tour = tours.find(el => el.id === id)
+           // res.status(200).json({
+           //            status: 'success',
+           //            data: {
+           //                       tour
+           //            }
+           // })
 }
 
 exports.updateTour = (req, res) => {
            const id = req.params.id * 1
-           if (id > tours.length) {
-                      return res.status(404).json({
-                                 status: 'fail',
-                                 message: 'Invalid Id'
-                      })
-           }
-           res.status(200).json({
-                      status: 'success',
-                      data: {
-                                 tour:'<Updated tour here>'
-                      }
-           })
+           // if (id > tours.length) {
+           //            return res.status(404).json({
+           //                       status: 'fail',
+           //                       message: 'Invalid Id'
+           //            })
+           // }
+           // res.status(200).json({
+           //            status: 'success',
+           //            data: {
+           //                       tour:'<Updated tour here>'
+           //            }
+           // })
 }
 
 exports.deleteTour = (req, res) => {
            const id = req.params.id * 1
-           if (id > tours.length) {
-                      return res.status(404).json({
-                                 status: 'fail',
-                                 message: 'Invalid Id'
-                      })
-           }
-           res.status(204).json({
-                      status: 'success',
-                      data: null
-           })
+           // if (id > tours.length) {
+           //            return res.status(404).json({
+           //                       status: 'fail',
+           //                       message: 'Invalid Id'
+           //            })
+           // }
+           // res.status(204).json({
+           //            status: 'success',
+           //            data: null
+           // })
 }
 
 exports.checkBody = (req, res, next) => {
--- server.js
+++ server.js
@@ -26,38 +26,6 @@ mongoose.connect(DB, {   // Connecting the application to the hosted DB
 
 // Model: Blueprint of documents. To perform CRUD we need models, for models we need schema we need schema to describe data. default value etc
 
-const tourSchema = new mongoose.Schema({ // Specify the datatypes and validation for our model.
-  name: {
-    type: String,
-    required: true,
-    unique: true
-  },
-  rating: {
-    type: Number,
-    default: 4.5
-  },
-  price: {
-    type: Number,
-    required: [true, 'A tour must have a price']
-  }
-});
-
-const Tour = mongoose.model('Tour', tourSchema)
-
-const testTour = new Tour({ // Document instance
-  name: 'The Forest Hiking',
-  rating: 4.7,
-  price: 497
-})
-
-testTour.save() // Save the database instance in database, returns a promise
-
-testTour.save().then(doc => {
-  console.log(doc);
-}).catch(err => {
-  console.log('Errororororororororororo', err);
-})
-
 const port = 3000;
 app.listen(port, () => {
   console.log(`App running on port ${port}...`);
