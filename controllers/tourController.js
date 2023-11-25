b93530d59d619e66455e3763b24463f4b98b862f
Ash-2k3 <ash.me.cse@gmail.com>
2023-11-25 14:08:22 +0530

    Modify update and get methods

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -2,15 +2,24 @@ const fs = require('fs')
 const { nextTick } = require('process')
 const Tour = require('./../models/tourModel')
 
-exports.getAllTours = (req, res) => {
+exports.getAllTours =  async (req, res) => {
+           try {
+                      const tours = await Tour.find() // All docs if no args passed
            res.status(200).json({
                       status: 'success',
                       requestAt: req.requestTime,
-                      // result: tours.length,
-                      // data: {
-                      //            tours: tours
-                      // }
+                      result: tours.length,
+                      data: {
+                                 tours: tours
+                      }
            })
+           } catch (err) {
+                      res.status(404).json({
+                                 status: 'fail',
+                                 message: err
+                      })
+           }
+           
 }
 
 exports.createTour = async (req, res) => {
@@ -31,41 +40,44 @@ exports.createTour = async (req, res) => {
 }
 }
 
-exports.getTourById = (req, res) => {  // ? for optional 
-           // console.log(req.body) // cuz of middleware body is accessible
-           console.log(req.params)
-           
-           // const id = req.params.id * 1
-           // if (id > tours.length) {
-           //            return res.status(404).json({
-           //                       status: 'fail',
-           //                       message: 'Invalid Id'
-           //            })
-           // }
-           
-           // const tour = tours.find(el => el.id === id)
-           // res.status(200).json({
-           //            status: 'success',
-           //            data: {
-           //                       tour
-           //            }
-           // })
+exports.getTourById = async (req, res) => {  // ? for optional 
+           try {
+                      const tour = await Tour.findById(req.params.id);
+                      res.status(200).json({
+                                 status: 'success',
+                                 data: {
+                                            tour
+                                 }
+                      })
+           } catch (err) {
+                      res.status(404).json({
+                                 status: 'fail',
+                                 message: err
+                      })
+           }
 }
 
-exports.updateTour = (req, res) => {
-           const id = req.params.id * 1
-           // if (id > tours.length) {
-           //            return res.status(404).json({
-           //                       status: 'fail',
-           //                       message: 'Invalid Id'
-           //            })
-           // }
-           // res.status(200).json({
-           //            status: 'success',
-           //            data: {
-           //                       tour:'<Updated tour here>'
-           //            }
-           // })
+exports.updateTour = async (req, res) => {
+           console.log('Update method is being called');
+           try {      
+                      console.log(req.body)
+                      const tour = await Tour.findByIdAndUpdate(
+                                 req.params.id,
+                                 req.body, {
+                                            new: true // We want to return the new document to the client
+                                 })
+                      res.status(200).json({
+                      status: 'success',
+                      data: {
+                                 tour
+                      }
+           })
+           } catch (err) {
+                      res.status(404).json({
+                                 status: 'fail',
+                                 message: 'Invalid Id'
+                      })
+           }
 }
 
 exports.deleteTour = (req, res) => {
