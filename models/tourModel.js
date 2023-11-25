a080b54e7b0793e0d5554ace6e4adb97c84c7bcc
Ash-2k3 <ash.me.cse@gmail.com>
2023-11-25 14:57:00 +0530

    Update the schema of tour model for section 8

--- models/tourModel.js
+++ models/tourModel.js
@@ -6,14 +6,50 @@ const tourSchema = new mongoose.Schema({ // Specify the datatypes and validation
              required: true,
              unique: true
            },
-           rating: {
+           duration: {
+            type: Number,
+            required: [true, 'A tour must have a duration']
+           },
+           maxGroupSize: {
+            type: Number,
+            require: [true, 'A tour must have a group size']
+           },
+           difficulty: {
+             type: String,
+             required: [true, 'A tour must have a difficulty']
+           },
+           ratingsAverage: {
              type: Number,
              default: 4.5
+           },
+           ratingsQuantity: {
+             type: Number,
+             default: 0
            },
            price: {
              type: Number,
              required: [true, 'A tour must have a price']
-           }
+           },
+           priceDiscount: Number,
+           summary: {
+             type: String,
+             trim: true, // Only works for string property (Removes white space from the string)
+            required: true
+            },
+           description: {
+             type: String,
+             trim: true
+           },
+           imageCover: {
+             type: String,
+             required: [true, 'A tour must have a cover image']
+           },
+           images: [String],
+           createdAt: {
+             type: Date,
+             default: Date.now() // Timestamp
+           },
+           startDate: [Date]
          });
 
 const Tour = mongoose.model('Tour', tourSchema)
