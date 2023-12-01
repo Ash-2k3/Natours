d7bac12d8a5a57fca6f1cf608e870aee4dbc6e16
Ash-2k3 <ash.me.cse@gmail.com>
2023-12-02 00:35:33 +0530

    Method two of filtering by using special methods provided by mongoose

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -5,10 +5,11 @@ const Tour = require('./../models/tourModel')
 exports.getAllTours =  async (req, res) => {
            try {
                       console.log(req.query);
-                      const tours = await Tour.find({ // We can filter out the documents by just passing the conditions inside as key value pair
-                                 duration: 5,
-                                 difficulty: 'easy'
-                      }) // All docs if no args passed
+                      const tours = await Tour.find()
+                      .where('duration')
+                      .equals(5)
+                      .where('difficulty')
+                      .equals('easy')
            res.status(200).json({
                       status: 'success',
                       requestAt: req.requestTime,
